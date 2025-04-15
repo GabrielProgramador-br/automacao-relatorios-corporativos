bae4462632a8257e9d41090fad9233c2435552c7
GabrielProgramador-br <gabrielferreira192000@gmail.com>
2025-04-15 18:18:57 -0300

    Estudo jinja2

---
+++ jinja.py
@@ -0,0 +1,26 @@
+import jinja2
+
+model = 'Olá ..., seja bem vindo. Seu email é... e seu estado: ...'
+
+dados = [
+    ['g@g.com', 'Gabriel', 'São Paulo'],
+    ['g@g.com', 'Gabriel', 'São Paulo'],
+    ['g@g.com', 'Gabriel', 'São Paulo']
+]
+
+template = jinja2.template()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
