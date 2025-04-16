a3ce314d9cedca3151ad9df0175b52c845e1076c
GabrielProgramador-br <gabrielferreira192000@gmail.com>
2025-04-16 15:55:54 -0300

    Refactor data loading and processing functions for improved clarity and efficiency

--- main.py
+++ main.py
@@ -1,86 +1,63 @@
+from Funções import tratamento_dados
+from Funções import funcoes_graficos
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import io
 import base64
 
-# Puxando os dados
-df_janeiro = pd.read_csv(r'Dados/Dados_janeiro_2025.csv', sep=';', encoding='ISO-8859-1')
-df_fevereiro = pd.read_csv(r'Dados/Dados_fevereiro_2025.csv', sep=';', encoding='ISO-8859-1')
-df_marco = pd.read_csv(r'Dados/Dados_marco_2025.csv', sep=';', encoding='ISO-8859-1')
-
-# Concatenando os dados
-df_2025 = pd.concat([df_janeiro, df_fevereiro, df_marco], ignore_index=True)
-
-# Removendo colunas que não são necessárias e limpando os dados
-colunas_zeradas = ['REPASSE', 'Cï¿½DIGO ï¿½RGï¿½O SUPERIOR', 'NOME ï¿½RGï¿½O SUPERIOR', 'Cï¿½DIGO ï¿½RGï¿½O', 'NOME ï¿½RGï¿½O', 'Cï¿½DIGO UNIDADE GESTORA', 'Mï¿½S EXTRATO', 'Nï¿½MERO CONVï¿½NIO', 'Cï¿½DIGO CONVENENTE', 'DATA TRANSAï¿½ï¿½O', 'VALOR TRANSAï¿½ï¿½O', 'TRANSAï¿½ï¿½O']
-df_2025.drop(columns=colunas_zeradas, inplace=True)
-df_2025.drop_duplicates(inplace=True)
-df_2025.dropna(inplace=True)
-
-# Convertendo os tipos de dados
-df_2025['DATA TRANSAÇÃO'] = pd.to_datetime(df_2025['DATA TRANSAÇÃO'], format='%d/%m/%Y')
-df_2025['VALOR TRANSAÇÃO'] = df_2025['VALOR TRANSAÇÃO'].str.replace('.', '', regex=False).str.replace(',', '.', regex=False).astype(float)
-
-colunas_numericas = ['CÓDIGO ÓRGÃO SUPERIOR', 'CÓDIGO ÓRGÃO', 'CÓDIGO UNIDADE GESTORA', 'MÊS EXTRATO', 'NÚMERO CONVÊNIO', 'CÓDIGO CONVENENTE']
-df_2025[colunas_numericas] = df_2025[colunas_numericas].astype(int)
-
-df = df_2025
-df["ANO_MES"] = df["DATA TRANSAÇÃO"].dt.to_period("M")
+# Configurando o estilo do seaborn
 sns.set(style="whitegrid")
 plt.rcParams["figure.figsize"] = (12, 6)
 
+# Puxando os dados
+df_janeiro, df_fevereiro, df_marco = tratamento_dados.carregar_dados([r'Dados/Dados_janeiro_2025.csv', r'Dados/Dados_fevereiro_2025.csv', r'Dados/Dados_marco_2025.csv'], ';', 'ISO-8859-1')
+
+# Concatenando os dados
+df = tratamento_dados.junta_dados([df_janeiro, df_fevereiro, df_marco])
+
+# limpando os dados
+df_2025 = tratamento_dados.limpar_dados(df)
+
+print(df_2025.columns)
+
+# Convertendo os tipos de dados
+df = tratamento_dados.converter_para_datetime(df, 'DATA TRANSACAO')
+df = tratamento_dados.converter_para_valor(df, 'VALOR TRANSACAO')
+colunas_numericas = ['CODIGO ORGAO SUPERIOR', 'CODIGO ORGAO', 'CODIGO UNIDADE GESTORA', 'MES EXTRATO', 'NUMERO CONVENIO', 'CODIGO CONVENENTE']
+df = tratamento_dados.converter_para_int(df, colunas_numericas)
+
+# Convertendo a coluna de data para o formato de ano e mês
+df["ANO_MES"] = df["DATA TRANSACAO"].dt.to_period("M")
+
 # Gráfico 1: Total de valor transacionado por mês
-df.groupby("ANO_MES")["VALOR TRANSAÇÃO"].sum().plot(kind="bar", title="Total de Valor Transacionado por Mês")
+df.groupby("ANO_MES")["VALOR TRANSACAO"].sum().plot(kind="bar", title="Total de Valor Transacionado por Mês")
 plt.xticks(rotation=45)
-plt.tight_layout()
-buf = io.BytesIO()
-plt.savefig(buf, format='png')
-buf.seek(0)
-image1 = base64.b64encode(buf.read()).decode('utf-8')
-print(image1)
-buf.close()
+image1 = funcoes_graficos.salvando_imagem(plt, "grafico1.png")
+
 
 # Gráfico 2: Top 10 Unidades Gestoras por valor transacionado
-df.groupby("NOME UNIDADE GESTORA")["VALOR TRANSAÇÃO"].sum().sort_values(ascending=False).head(10).plot(kind="bar", title="Top 10 Unidades Gestoras por Valor Transacionado")
+df.groupby("NOME UNIDADE GESTORA")["VALOR TRANSACAO"].sum().sort_values(ascending=False).head(10).plot(kind="bar", title="Top 10 Unidades Gestoras por Valor Transacionado")
 plt.xticks(rotation=90)
-plt.tight_layout()
-buf = io.BytesIO()
-plt.savefig(buf, format='png')
-buf.seek(0)
-image1 = base64.b64encode(buf.read()).decode('utf-8')
-print(image1)
-buf.close()
+image2 = funcoes_graficos.salvando_imagem(plt, "grafico2.png")
 
 # Gráfico 3: Top 10 Favorecidos por valor transacionado
-df.groupby("NOME FAVORECIDO")["VALOR TRANSAÇÃO"].sum().sort_values(ascending=False).head(10).plot(kind="bar", title="Top 10 Favorecidos por Valor Transacionado")
+df.groupby("NOME FAVORECIDO")["VALOR TRANSACAO"].sum().sort_values(ascending=False).head(10).plot(kind="bar", title="Top 10 Favorecidos por Valor Transacionado")
 plt.xticks(rotation=90)
-plt.tight_layout()
-buf = io.BytesIO()
-plt.savefig(buf, format='png')
-buf.seek(0)
-image1 = base64.b64encode(buf.read()).decode('utf-8')
-print(image1)
-buf.close()
+image3 = funcoes_graficos.salvando_imagem(plt, "grafico3.png")
 
 # Gráfico 4: Distribuição dos valores de transação
-sns.histplot(df["VALOR TRANSAÇÃO"].dropna(), bins=50, kde=True)
+sns.histplot(df["VALOR TRANSACAO"].dropna(), bins=50, kde=True)
 plt.title("Distribuição dos Valores de Transação")
-plt.tight_layout()
-buf = io.BytesIO()
-plt.savefig(buf, format='png')
-buf.seek(0)
-image1 = base64.b64encode(buf.read()).decode('utf-8')
-print(image1)
-buf.close()
+image4 = funcoes_graficos.salvando_imagem(plt, "grafico4.png")
 
 # Gráfico 5: Evolução temporal do valor transacionado
-df.groupby("DATA TRANSAÇÃO")["VALOR TRANSAÇÃO"].sum().plot(kind="line", title="Evolução Temporal do Valor Transacionado")
-plt.tight_layout()
-buf = io.BytesIO()
-plt.savefig(buf, format='png')
-buf.seek(0)
-image1 = base64.b64encode(buf.read()).decode('utf-8')
-print(image1)
-buf.close()
+df.groupby("DATA TRANSACAO")["VALOR TRANSACAO"].sum().plot(kind="line", title="Evolução Temporal do Valor Transacionado")
+image5 = funcoes_graficos.salvando_imagem(plt, "grafico5.png")
 
+print(image1)
+print(image2)
+print(image3)
+print(image4)
+print(image5)
